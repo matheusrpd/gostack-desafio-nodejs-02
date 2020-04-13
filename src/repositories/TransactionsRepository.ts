fce4d0e5152b26ded7d6b9a9e32740a685bb6714
matheusrpd <matheusrpd@gmail.com>
2020-04-13 18:43:20 -0300

    Desafio finalizado.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let valueTotalInCome = 0;
+    let valueTotalOutCome = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        valueTotalInCome += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        valueTotalOutCome += transaction.value;
+      }
+    });
+
+    const balance = {
+      income: valueTotalInCome,
+      outcome: valueTotalOutCome,
+      total: valueTotalInCome - valueTotalOutCome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
